4346737a101d7b578fcf14e60e6a2a2b416fbae4
Collin Barlow <Collin@Collins-MacBook-Pro.local>
2019-11-07 15:17:52 -0600

    The code now reverts back much like the calculator does now, it as well will stop with an incorrect input .

--- EvenOdd.py
+++ EvenOdd.py
@@ -1,10 +1,22 @@
-def even_or_odd():
+def ask_user_input():
     greet = "Hello please enter an integer!"
     print(greet)
+
+ask_user_input()
+
+def user_input():
     number = int(input("Integer: "))
     return number
 
-if (even_or_odd() % 2 == 0):
-    print("That number is even.")
-else:
-    print("That number is odd.")
+def even_or_odd():
+    if (user_input() % 2 == 0):
+        print("is an even number.")
+        print("------------------")
+    else:
+        print("is an odd number.")
+        print("-----------------")
+    return even_or_odd()
+
+even_or_odd()
+
+user_input()
