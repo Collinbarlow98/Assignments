93a81f3666b02022f724599d3143cb7c0754057e
Collin Barlow <Collin@Collins-MacBook-Pro.local>
2019-11-07 14:48:02 -0600

    The process now reverts back to the start unless incorrect inputs are done.

--- Calculator.py
+++ Calculator.py
@@ -1,15 +1,44 @@
-first_number = int(input("First Number: "))
-operand = input("Operand: ")
-second_number = int(input("Second Number: "))
-if operand == "+":
-    sum = first_number + second_number
-    print(sum)
-if operand == "-":
-    difference = first_number - second_number
-    print(difference)
-if operand == "*":
-    product = first_number * second_number
-    print(product)
-if operand == "/":
-    quotient = first_number / second_number
-    print(quotient)
+def calculate():
+    first_number = int(input("First Number: "))
+    operand = input("Operand: ")
+    second_number = int(input("Second Number: "))
+
+    if operand == "+":
+        print(first_number + second_number)
+    elif operand == "-":
+        print(first_number - second_number)
+    elif operand == "*":
+        print(first_number * second_number)
+    elif operand == "/":
+        print(first_number / second_number)
+    else:
+        print("Incorrect input")
+    return calculate()
+
+calculate()
+
+#def ask_user_for_input():
+#    first_number = int(input("First Number: "))
+#    operand = input("Operand: ")
+#    second_number = int(input("Second Number: "))
+#    return(first_number, operand, second_number)
+#
+#def add(a,b):
+#    return a+b
+#
+#def subtract(a,b):
+#    return a-b
+#
+#def display_result(a, b, op, result):
+#    print(f"{a} {op} {b} = {result})
+#
+#(no1, op, no2) = ask_user_for_input()
+#
+#if operand == "+":
+#        print(first_number + second_number)
+#elif operand == "-":
+#        print(first_number - second_number)
+#elif operand == "*":
+#        print(first_number * second_number)
+#elif operand == "/":
+#        print(first_number / second_number)
